498796f18ac23e46cb6f37a479f387f647e9c428
Musaab Elfaqih <musabelfaki@gmail.com>
2020-05-09 17:31:14 +0300

    Handle larger screens

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -3,13 +3,11 @@
   import { key } from "../actions/key";
   import { away } from "../actions/away";
   import Nav from "../components/Nav.svelte";
-
-  export let segment;
 </script>
 
 <div class="min-h-screen border-t-8 border-teal-700">
 
-  <div class="max-w-7xl mx-auto px-4 py-10 sm:px-6 lg:px-8">
+  <div class="max-w-7xl mx-auto px-4 py-10 sm:max-w-2xl sm:px-6 lg:px-8">
     <slot></slot>
   </div>
 </div>
