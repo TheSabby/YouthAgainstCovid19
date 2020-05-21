631bcaeac49cedea82bc1f96a44d32ca5469407d
Musaab Elfaqih <musabelfaki@gmail.com>
2020-05-21 22:26:36 +0300

    Improve Africa Day page styles

--- src/routes/africaDay/index.svelte
+++ src/routes/africaDay/index.svelte
@@ -1,54 +1,101 @@
 <script>
-  import MobileMenu from '../../components/MobileMenu.svelte';
+  import MobileMenu from "../../components/MobileMenu.svelte";
 </script>
 
-<div class="relative bg-white">
-  <div
-    class="flex justify-between items-center px-4 py-6 sm:px-6 md:justify-start
-    md:space-x-10">
-    <div>
-      <a href="/" class="text-2xl font-extrabold text-black tracking-tighter hover:text-black active:text-black">
-        NexusAfrik
-        <!-- <img class="h-8 w-auto sm:h-10" src="/logo-512.png" alt="Workflow" /> -->
-      </a>
-    </div>
-    <div class="-mr-2 -my-2 md:hidden">
-      <MobileMenu />
-    </div>
+<style>
+  .top {
+    background: rgb(52, 143, 65);
+    background: radial-gradient(
+      circle,
+      rgba(52, 143, 65, 1) 52%,
+      rgba(32, 97, 41, 1) 100%
+    );
+  }
+
+  .support-btn {
+    background-color: #eaa739;
+  }
+
+  .support-btn:hover {
+    background-color: #ffbd50;
+  }
+
+  .support-btn:active {
+    background-color: #d89a35;
+  }
+</style>
+
+<div class="top pb-8">
+  <div class="relative">
     <div
-      class="hidden md:flex-1 md:flex md:items-center md:justify-between
-      md:space-x-12">
-      <nav class="flex space-x-10">
+      class="flex justify-between items-center px-4 py-6 sm:px-6
+      md:justify-start md:space-x-10">
+      <div>
         <a
-          href="/africaDay"
-          class="text-base leading-6 font-medium text-gray-500
-          hover:text-gray-900 focus:outline-none focus:text-gray-900 transition
+          href="/"
+          class="text-base leading-6 font-medium text-gray-50
+          hover:text-gray-300 focus:outline-none focus:text-gray-900 transition
           ease-in-out duration-150">
-          Africa Day
+          Home
         </a>
-      </nav>
+      </div>
+      <div class="-mr-2 -my-2 md:hidden">
+        <MobileMenu />
+      </div>
+      <div
+        class="hidden md:flex-1 md:flex md:items-center md:justify-between
+        md:space-x-12">
+        <nav class="flex space-x-10">
+          <a
+            href="/africaDay"
+            class="text-base leading-6 font-medium text-gray-50
+            hover:text-gray-300 focus:outline-none focus:text-gray-900
+            transition ease-in-out duration-150">
+            Africa Day
+          </a>
+        </nav>
+      </div>
     </div>
+  </div>
+
+  <h1
+    style="line-height: 3.5rem"
+    class="mt-6 px-1 text-white text-center text-5xl font-extrabold
+    tracking-tighter sm:text-6xl sm:font-extrabold md:leading-10
+    sm:tracking-tight">
+    Together for a
+    <span style="color: #eaa739">stronger Africa</span>
+  </h1>
+
+  <div class="mt-4 flex items-center justify-center p-4">
+    <div
+      style="height: 576px; width: 1024px; max-width: 90%; background: #000" />
+    <!-- <iframe
+      title="Africa Day videos"
+      width="1024"
+      height="576"
+      src="https://www.youtube-nocookie.com/embed/4tYjRp2pl3c"
+      frameborder="0"
+      allow="accelerometer; autoplay; encrypted-media; gyroscope;
+      picture-in-picture"
+      allowfullscreen /> -->
+  </div>
+
+  <div class="mt-4 flex justify-center">
+    <span class="inline-flex rounded-md shadow-sm">
+      <button
+        type="button"
+        class="support-btn inline-flex items-center px-6 py-3 border
+        border-transparent text-2xl tracking-tight leading-6 font-semibold rounded-md text-white
+        focus:outline-none transition ease-in-out duration-150">
+        Support Us
+      </button>
+    </span>
   </div>
 </div>
 
-<div class="flex items-center justify-center p-4 video-area">
-  <iframe
-    title="Africa Day videos"
-    width="1024"
-    height="576"
-    src="https://www.youtube-nocookie.com/embed/4tYjRp2pl3c"
-    frameborder="0"
-    allow="accelerometer; autoplay; encrypted-media; gyroscope;
-    picture-in-picture"
-    allowfullscreen />
-</div>
-
-<img class="hidden sm:block" src="/AfricaDay_BannerLarge.png" alt="Africa Day" />
+<img
+  class="hidden sm:block"
+  src="/AfricaDay_BannerLarge.png"
+  alt="Africa Day" />
 <img class="sm:hidden" src="/AfricaDay_BannerMobile.png" alt="Africa Day" />
-
-<style>
-  .video-area {
-    background: rgb(52,143,65);
-    background: radial-gradient(circle, rgba(52,143,65,1) 52%, rgba(32,97,41,1) 100%);
-  }
-</style>
