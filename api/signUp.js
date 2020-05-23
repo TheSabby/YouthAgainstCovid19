700ca24c13bfdc9f9736f3506eebd3c18b0f171a
Musaab Elfaqih <musabelfaki@gmail.com>
2020-05-23 10:40:25 +0300

    Attempt setting a doc

--- api/signUp.js
+++ api/signUp.js
@@ -1,6 +1,11 @@
 const { db } = require("./_utils/firebase");
+const signUpSubmissionsRef = db.collection('sign_up_submissions');
 
 module.exports = (req, res) => {
+  const submission = signUpSubmissionsRef.add({
+    test: true
+  });
+
   res.json({
     body: req.body,
     query: req.query,
