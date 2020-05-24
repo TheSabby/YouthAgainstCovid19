2768ed6a826b5807b36300448ad3cada7e92d9dc
Musaab Elfaqih <musabelfaki@gmail.com>
2020-05-24 16:06:07 +0300

    now csv

--- api/getCSV.js
+++ api/getCSV.js
@@ -1,17 +1,30 @@
 const { db, admin } = require("./_utils/firebase");
 const newsletterSignUps = db.collection("newsletter_sign_ups");
+const moment = require("moment");
+
+const fields = ["email", "date"];
 
 module.exports = async (req, res) => {
   const allNewsletterSignUps = await newsletterSignUps
     .get()
     .then((snapshot) => snapshot.docs.map((doc) => doc.data()))
+    .then((signUps) =>
+      signUps.map((signUp) => ({
+        email: signUp.email,
+        date: moment.unix(signUp.date._seconds),
+      }))
+    )
     .catch((err) => {
       console.error("Error getting documents", err);
       return {
         err,
-        failed: true
-      }
+        failed: true,
+      };
     });
 
-  res.json(allNewsletterSignUps);
+  json2csv({ data: allNewsletterSignUps, fields }, function (err, csv) {
+    res.setHeader("Content-disposition", "attachment; filename=data.csv");
+    res.set("Content-Type", "text/csv");
+    res.status(200).send(csv);
+  });
 };
